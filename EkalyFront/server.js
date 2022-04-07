3f8f5b8ad566e24ec08adb7257e8b8220341d4f3
andrianinaChristelle <72926040+andrianinaChristelle@users.noreply.github.com>
2022-04-07 11:39:36 +0300

    debug

--- EkalyFront/server.js
+++ EkalyFront/server.js
@@ -5,5 +5,4 @@ app.use(express.static(__dirname + "/dist/ekaly-front"));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/ekaly-front/index.html"));
 });
-app.listen(process.env.PORT || 8080);
-<ekaly-front></ekaly-front>;
+app.listen(process.env.PORT);
